439c871ff6da603cc180c8f80b63a07495b9b70c
NadezhdaTrofimova <trofimnad1991@gmail.com>
2021-08-26 00:51:31 +0300

    add inputs via options

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,17 +1,30 @@
 import styles from './Input.module.css'
 
 
-const Input = () => {
+const Input = ({optionsInput}) => {
+
+    // const parametrs = [
+    //     {placeholder: 'Имя', icon: './../../image/inputIcons/iconForNameForname.png', visibility: 'visible', hidden: false},
+    //     {placeholder: 'Фамилия', icon: './../../image/inputIcons/iconForNameForname.png', visibility: 'visible', hidden: false},
+    //     {placeholder: 'Email',  icon: './../../image/inputIcons/iconForEmail.png', visibility: 'visible', hidden: false},
+    //     {placeholder: 'Пароль',  icon: './../../image/inputIcons/iconForPassword.png', visibility: 'visible', hidden: false},
+    //     {placeholder: 'Повторите пароль', icon: './../../image/inputIcons/iconForPassword.png', visibility: 'visible', hidden: false},
+    // ]
+
+
     return (
         <>
-            <div className={styles.mainInputContainer}>
+            {optionsInput.map(param =>
+            <div className={styles.mainInputContainer} visibility={param.visibility} hidden={param.hidden}>
                 <div className={styles.iconBackground}>
-                    <img className={styles.icon} src='./../../image/inputIcons/iconForNameForname.png'/>
+                    <img className={styles.icon} src={param.icon}/>
                 </div>
                 <div className={styles.inputContainer}>
-                    <input className={styles.input} placeholder='Имя'/>
+
+                    <input className={styles.input} placeholder={param.placeholder}/>
                 </div>
             </div>
+            )}
         </>
     )
 }
