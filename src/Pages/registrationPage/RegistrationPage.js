b561973a6c41d0e969ce086f3582956141095962
NadezhdaTrofimova <trofimnad1991@gmail.com>
2021-08-27 13:40:31 +0300

    add icon input

--- src/Pages/registrationPage/RegistrationPage.js
+++ src/Pages/registrationPage/RegistrationPage.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ModalWindow from "../../components/modalWindow/ModalWindow";
+import image1 from './../../image/inputIcons/iconForNameForname.png'
+import image2 from './../../image/inputIcons/iconForEmail.png'
+import image3 from './../../image/inputIcons/iconForPassword.png'
 
 
 const RegistrationPage = () => {
@@ -9,35 +12,35 @@ const RegistrationPage = () => {
             id: 1,
             placeholder: 'Имя',
             type: 'text',
-            icon: './../../image/inputIcons/iconForNameForname.png',
+            icon: image1,
             isNotVisibility: false
         },
         {
             id: 2,
             placeholder: 'Фамилия',
             type: 'text',
-            icon: './../../image/inputIcons/iconForNameForname.png',
+            icon: image1,
             isNotVisibility: false
         },
         {
             id: 3,
             placeholder: 'Email',
             type: 'email',
-            icon: './../../image/inputIcons/iconForEmail.png',
+            icon: image2,
             isNotVisibility: false
         },
         {
             id: 4,
             placeholder: 'Пароль',
             type: 'password',
-            icon: './../../image/inputIcons/iconForPassword.png',
+            icon: image3,
             isNotVisibility: false
         },
         {
             id: 5,
             placeholder: 'Повторите пароль',
             type: 'password',
-            icon: './../../image/inputIcons/iconForPassword.png',
+            icon: image3,
             isNotVisibility: false
         },
     ]
