72ed870240ee3cc793064fca9ae10af5a1bb2c3d
NadezhdaTrofimova <trofimnad1991@gmail.com>
2021-09-11 13:51:48 +0300

    install and do settings react-route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import LoginPage from "./views/login-page";
 import RegistrationPage from "./views/registration-page";
+import {Route, Link} from "react-router-dom";
 
 
 function App() {
     return (
         <div className='App'>
-            <RegistrationPage/>
+            <div>
+                <Link to='/registration'>Reg</Link>
+            </div>
+            <div>
+                <Link to='/'>Log</Link>
+            </div>
+            <Route path='/registration'>
+                <RegistrationPage/>
+            </Route>
+            <Route path='/'>
+                <LoginPage/>
+            </Route>
         </div>
     );
 }
---
+++ src/views/login-page/LoginPage.js
@@ -0,0 +1,18 @@
+import ModalWindow from "../../components/modalWindow/ModalWindow";
+import TopForm from "../../components/topForm/TopForm";
+import Form from "../../components/form/Form";
+import Input from "../../components/input/Input";
+import Button from "../../components/button/Button";
+
+
+const LoginPage = () => {
+    return (
+        <>
+            <TopForm/>
+            {/*<Input/>*/}
+            <Button/>
+        </>
+    )
+}
+
+export default LoginPage
