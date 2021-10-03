0f7a213e880b2a6516cff2af808d2ddc7d46f1d2
NadezhdaTrofimova <trofimnad1991@gmail.com>
2021-10-03 07:00:50 +0300

    add largeInput component

--- src/views/chat-search-page/SearchChatPage.js
+++ src/views/chat-search-page/SearchChatPage.js
@@ -1,7 +1,10 @@
 import React from "react";
+import styles from './SearchChatPage.module.css'
+import iconButton from './../../image/largeInputIcons/icon-magnifier.png'
 import Header from "../../components/header/Header";
 import iconSettings from '../../image/headerIcons/icon-settings.png'
 import iconUser from '../../image/headerIcons/icon-user1.png'
+import LargeInput from "../../components/largeInput/LargeInput";
 
 const SearchChatPage = () => {
 
@@ -12,6 +15,17 @@ const SearchChatPage = () => {
                 iconVariable={iconSettings}
                 title='Настройки'
             />
+            <div className={styles.inputContainer}>
+                <LargeInput
+                    title='Поиск чата'
+                    buttonContent={
+                        <div className={styles.buttonContainer}>
+                            <img src={iconButton} alt="button-icon" className={styles.buttonIcon}/>
+                            <p className={styles.buttonTitle}>Найти</p>
+                        </div>
+                    }
+                />
+            </div>
         </>
     )
 }
--- src/views/messages-page/MessagesPage.js
+++ src/views/messages-page/MessagesPage.js
@@ -1,7 +1,12 @@
 import React from "react";
-import Header from "../../components/header/Header";
+import styles from './MessagesPage.module.css'
 import iconSettings from '../../image/headerIcons/icon-settings.png'
 import iconUser from '../../image/headerIcons/icon-user1.png'
+import iconEmoji from '../../image/largeInputIcons/icon-emoji.png'
+import iconAttach from '../../image/largeInputIcons/icon-clip.png'
+import iconSendMessage from '../../image/largeInputIcons/icon-paper-plane.png'
+import Header from "../../components/header/Header";
+import LargeInput from "../../components/largeInput/LargeInput";
 
 const MessagesPage = () => {
 
@@ -12,6 +17,21 @@ const MessagesPage = () => {
                 iconVariable={iconSettings}
                 title='Настройки'
             />
+            <div className={styles.inputContainer}>
+                <LargeInput
+                    className={styles.inputContainer}
+                    title='Введите Ваше сообщение'
+                    buttonContent={
+                        <div className={styles.buttonContainer}>
+                            <img className={styles.icon} src={iconEmoji} alt="emoji-icon"/>
+                            <img className={styles.icon} src={iconAttach} alt="magnifier-icon"/>
+                            <div className={styles.iconSendMessageWrapper}>
+                                <img className={styles.icon} src={iconSendMessage} alt="paper-plane-icon"/>
+                            </div>
+                        </div>
+                    }
+                />
+            </div>
         </>
     )
 }
