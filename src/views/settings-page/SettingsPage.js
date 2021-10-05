4500efb992dd631afaca678322b537c6fc8a779a
NadezhdaTrofimova <trofimnad1991@gmail.com>
2021-10-05 07:46:22 +0300

    add settings page

--- src/views/settings-page/SettingsPage.js
+++ src/views/settings-page/SettingsPage.js
@@ -2,16 +2,66 @@ import React from "react";
 import Header from "../../components/header/Header";
 import iconSave from '../../image/icons/icon-disc.png';
 import iconUser from '../../image/usersPhoto/icon-user2.png'
+import photoUser from '../../image/usersPhoto/photo-user2.png'
+import styles from './SettingsPage.module.css'
+import iconEdit from '../../image/icons/icon-pencil.png'
 
 const SettingsPage = () => {
 
     return (
         <>
-            <Header
-                iconUser={iconUser}
-                iconVariable={iconSave}
-                title='Сохранить'
-            />
+            <div className={styles.settingsPage}>
+                <Header
+                    iconUser={iconUser}
+                    iconVariable={iconSave}
+                    title='Сохранить'
+                />
+                <section className={styles.mainContainer}>
+                    <div className={styles.photoContainer}>
+                        <img className={styles.userPhoto} src={photoUser} alt="user-photo"/>
+                        <a className={styles.changePhotoTitle}>Изменить фото</a>
+                    </div>
+                    <div className={styles.formContainer}>
+                        <div className={styles.inputContainer}>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                placeholder="Имя"
+                            />
+                            <img
+                                src={iconEdit}
+                                alt="icon-edit"
+                                className={styles.imageEdit}
+                            />
+                        </div>
+                        <div className={styles.inputContainer}>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                placeholder="Фамилия"
+                            />
+                            <img
+                                src={iconEdit}
+                                alt="icon-edit"
+                                className={styles.imageEdit}
+                            />
+                        </div>
+                        <div className={styles.inputContainer}>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                placeholder="E-mail"
+                            />
+                            <img
+                                src={iconEdit}
+                                alt="icon-edit"
+                                className={styles.imageEdit}
+                            />
+                        </div>
+                        <button className={styles.button}>Сохранить изменения</button>
+                    </div>
+                </section>
+            </div>
         </>
     )
 }
