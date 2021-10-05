01dfc9ba347a8fcf266a3cf55d0588d15f874a0a
NadezhdaTrofimova <trofimnad1991@gmail.com>
2021-10-05 05:08:59 +0300

    add chat page

--- src/views/chat-search-page/SearchChatPage.js
+++ src/views/chat-search-page/SearchChatPage.js
@@ -4,11 +4,13 @@ import Header from "../../components/header/Header";
 import iconSettings from '../../image/icons/icon-gear.png'
 import iconUser from '../../image/usersPhoto/icon-user1.png'
 import SearchInput from "../../components/searchInput/SearchInput";
+import MessageItem from "../../components/messageItem/MessageItem";
+import iconButton from "../../image/icons/icon-magnifier.png";
 
 const SearchChatPage = () => {
 
     return (
-        <>
+        <div className={styles.searchChatPage}>
             <Header
                 iconUser={iconUser}
                 iconVariable={iconSettings}
@@ -17,7 +19,20 @@ const SearchChatPage = () => {
             <div className={styles.inputContainer}>
                 <SearchInput/>
             </div>
-        </>
+            <div className={styles.messageMainContainer}>
+                <ul className={styles.messageContainer}>
+                    <MessageItem/>
+                    <MessageItem/>
+                    <MessageItem/>
+                    <MessageItem/>
+                    <MessageItem/>
+                    <MessageItem/>
+                </ul>
+                <div className={styles.buttonContainer}>
+                    <p className={styles.buttonTitle}>Создать чат</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
